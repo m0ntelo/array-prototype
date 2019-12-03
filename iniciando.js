69807161db27903fa82c4fbd0a06a2b2f35e6f2e
Wilson Montelo <wilson.montelo@vibedesenv.com>
2019-12-03 15:34:35 -0300

    instruções iniciais para criar um array e fazer manipulações

---
+++ iniciando.js
@@ -0,0 +1,13 @@
+
+const frutas = ['maça', 'banana', 'morango'];
+
+// frutas.push('laranja'); // adiciona um item ao final 
+// frutas.pop(); // remove item do final
+// frutas.shift(); // remove do inicio do array
+// frutas.unshift('melancia'); // adicionar ao inicio do array
+// const i = frutas.indexOf('banana'); // procurar indice de um item do array
+// frutas.splice(2, 1); // remover 1 elemento a partir do indice 2 
+// const copy = frutas.slice(); // copia o array
+
+// console.log(frutas);
+

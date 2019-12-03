5c91b1dbe3e2ec9c4a5419d111d1de56cd82272d
Wilson Montelo <wilson.montelo@vibedesenv.com>
2019-12-03 17:57:24 -0300

    filter() pecorre cada item do array e retorna true para o item que correspoder a função passada como callback

---
+++ filter.js
@@ -0,0 +1,12 @@
+/*
+ * filter=() pecorre cada item do array. e retorna true 
+ * para o item que corrensponder a função passada como callback
+ */
+
+// function isEnough(value) {
+//   return value >= 10;
+// }
+
+// const filter = [12, 5, 8, 130, 44].filter(isEnough);
+
+// console.log(filter);

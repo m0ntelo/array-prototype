0bff0d1d128b235fa9b486c6397bda52e315d1f0
Wilson Montelo <wilson.montelo@vibedesenv.com>
2019-12-03 15:56:45 -0300

    Concat cria um novo array unindo os elementos que foram passados como parâmetro

---
+++ concat.js
@@ -0,0 +1,12 @@
+/*
+ * Concat cria um novo array unindo os elementos que 
+ * foram passados como parâmetro
+ */
+ 
+// const a = [1, 2, 3];
+// const b = [4, 5, 6];
+// const c = [7, 8, 9];
+
+// const d = a.concat(b, c);
+
+// console.log(d);
